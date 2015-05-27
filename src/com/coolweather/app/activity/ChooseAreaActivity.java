55ed7c5ca42f58cf8a08d1d191bf6469ec284dbd
syy <1172069959@qq.com>
2015-05-27 21:43:50 +0800

    Brach1

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -27,6 +27,7 @@ import android.widget.Toast;
 
 public class ChooseAreaActivity extends Activity {
 
+
 	public static final int LEVEL_PROVINCE = 0;
 	public static final int LEVEL_CITY = 1;
 	public static final int LEVEL_COUNTY = 2;
